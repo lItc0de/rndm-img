6c2706926df4ffd1e34043fdd35b8cca11cb5866
litc0de <litc0de@posteo.org>
2022-01-21 17:36:23 +0100

    Updates

---
+++ component---src-pages-index-js-87bddafd25e9545a768e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprocessing_test=self.webpackChunkprocessing_test||[]).push([[678],{7704:function(e,n,t){t.r(n);var l=t(7294),a=(0,l.lazy)((function(){return Promise.all([t.e(6),t.e(503)]).then(t.bind(t,1105))}));n.default=function(){var e="undefined"==typeof window;return l.createElement("main",null,l.createElement("title",null,"Home Page"),!e&&l.createElement(l.Suspense,{fallback:l.createElement("div",null)},l.createElement(a,null)))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-87bddafd25e9545a768e.js.map
